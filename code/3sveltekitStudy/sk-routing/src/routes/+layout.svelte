e106ca8b96def2ba7975432aa9c0cae3c6ca082b
witwint <witwint1234@gmail.com>
2023-06-30 21:18:03 +0900

    라우트레이아웃 다양한 단계에서 공통 레이아웃 만들기

---
+++ code/3sveltekitStudy/sk-routing/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<header class="layout-header"> 해더영역 </header>
+
+<slot />
+
+<footer class="layout-footer"> 푸터 영역 </footer>
+
+<style>
+  .layout-header {
+    background-color: aqua;
+  }
+
+  .layout-footer {
+    background-color: aqua;
+  }
+  
+  
+
+</style>
---
+++ code/3sveltekitStudy/sk-routing/src/routes/products/[productId=integer]/+layout.svelte
@@ -0,0 +1,9 @@
+
+
+<slot />
+
+<hr>
+<h3>제품 배송, 반품 등</h3>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam blanditiis nisi voluptatum alias saepe, hic illum laboriosam facilis ipsa corporis quia repellendus eum architecto quae, amet atque laborum incidunt nulla.
+</p>
