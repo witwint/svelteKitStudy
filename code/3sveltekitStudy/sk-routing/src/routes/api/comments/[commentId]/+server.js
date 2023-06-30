f7583c2305f46cd1e7a5e2439d30e48f5f20bf80
witwint <witwint1234@gmail.com>
2023-07-01 01:03:02 +0900

    api delete

--- code/3sveltekitStudy/sk-routing/src/routes/api/comments/[commentId]/+server.js
+++ code/3sveltekitStudy/sk-routing/src/routes/api/comments/[commentId]/+server.js
@@ -11,3 +11,28 @@ export function GET(requestEvent) {
 
   return json(findedComment);
 }
+
+export async function PATCH(requestEvent) {
+  const { params, request} = requestEvent;
+  const { commentId } = params;
+  const { text } = await request.json();
+
+  //db에서 비즈니스 로직 수행 (db의 어떤 특정 댓글의 내용을 수정한다.)
+
+  const findedComment = comments.find(
+    (comment) => comment.id === parseInt(commentId)
+  )
+  findedComment.text = text;
+
+  return json(findedComment);
+}
+
+export async function DELETE(requestEvent) {
+  const { params } = requestEvent;
+  const { commentId } = params;
+
+  const deletedComment = comments.find((comment) => comment.id === parseInt(commentId))
+  const index = comments.findIndex((comment) => comment.id === parseInt(commentId));
+  comments.splice(index, 1); //비즈니스 로직 (삭제)
+  return json(deletedComment);
+}
