5933dcd21764773dc147544f3bd4f2a368b0e0c9
witwint <witwint1234@gmail.com>
2023-06-30 18:39:47 +0900

    옵션 파라미터

---
+++ code/3sveltekitStudy/sk-routing/src/routes/[[lang]]/docs/[...slug]/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { page } from '$app/stores';
+  console.log($page.params.slug);
+
+  const slugArr = $page.params.slug.split('/');
+  console.log(slugArr)
+
+  console
+  console.log($page.params);
+  const { lang = 'ko' } = $page.params;
+
+</script>
+
+{#if lang === 'ko'}
+  <h1>안녕</h1>
+{:else if lang === 'en'}
+  <h1>hello</h1>
+{:else if lang === 'ja'}
+  <h1>일본어</h1>
+  
+{/if}
+
+<hr/>
+<h1>Doce 페이지</h1>
+
+<!-- 스벨트문법 -->
+
+{#if slugArr.length === 1}
+<h1>Docs 페이지 ; 주제 {slugArr[0]}</h1>
+{:else if slugArr.length === 2}
+<h1>Docs 페이지 ; 주체 {slugArr[0]} ; 소주제 {slugArr[1]}</h1>
+{/if}
