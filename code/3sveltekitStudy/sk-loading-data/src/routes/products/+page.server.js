815ee41ca22678b7dcc01c4d6abc8cc8d3d45267
witwint <witwint1234@gmail.com>
2023-07-01 11:57:39 +0900

    server함수로만 데이터

--- code/3sveltekitStudy/sk-loading-data/src/routes/products/+page.server.js
+++ code/3sveltekitStudy/sk-loading-data/src/routes/products/+page.server.js
@@ -1,5 +1,6 @@
-export const load = async (loadEvent) => {
-  const {fetch} = loadEvent;
+export const load = async (serverloadEvent) => {
+  console.log("server load 함수 실행됨")
+  const {fetch} = serverloadEvent;
   const response = await fetch("http://localhost:4000/products");
   const products = await response.json();
 
